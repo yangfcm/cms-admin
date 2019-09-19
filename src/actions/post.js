c43924b6f5b42391f74e003c2a6a6c29df7d4db7
Fan Yang <yangfcm16@gmail.com>
2019-09-19 13:46:05 +0930

    Add all to fetch all posts

--- src/actions/post.js
+++ src/actions/post.js
@@ -1,127 +1,122 @@
-import axios from '../settings';
-import Cookies from 'js-cookie';
+import axios from "../settings";
+import Cookies from "js-cookie";
 
 import {
-	CREATE_POST,
-	DELETE_POST,
-	UPDATE_POST,
-	READ_POSTS,
-	READ_POST,
-	OPER_POST_ERR,
-	CLEAR_POST
-} from './types';
+  CREATE_POST,
+  DELETE_POST,
+  UPDATE_POST,
+  READ_POSTS,
+  READ_POST,
+  OPER_POST_ERR,
+  CLEAR_POST
+} from "./types";
 
 /**
  * Create a post
  */
-export const createPost = (postData) => {
-	return async(dispatch) => {
-		try {
-			const token = Cookies.get('admin_token');
-			const response = await axios.post(
-				'/api/posts',
-				postData,
-				{ headers: {'x-auth': `Bearer ${token}`}}
-			);
-			dispatch({
-				type: CREATE_POST,
-				payload: response.data
-			});
-		} catch(e) {
-			dispatch({
-				type: OPER_POST_ERR,
-				payload: e.response.data
-			})
-		}
-	}
-}
+export const createPost = postData => {
+  return async dispatch => {
+    try {
+      const token = Cookies.get("admin_token");
+      const response = await axios.post("/api/posts", postData, {
+        headers: { "x-auth": `Bearer ${token}` }
+      });
+      dispatch({
+        type: CREATE_POST,
+        payload: response.data
+      });
+    } catch (e) {
+      dispatch({
+        type: OPER_POST_ERR,
+        payload: e.response.data
+      });
+    }
+  };
+};
 
 /** Delete a post by id */
-export const deletePost = (id) => {
-	return async (dispatch) => {
-		try {
-			const token = Cookies.get('admin_token');
-			const response = await axios.delete(
-				`/api/posts/${id}`,
-				{ headers: {'x-auth': `Bearer ${token}` }}
-			);
-			dispatch({
-				type: DELETE_POST,
-				payload: response.data
-			});
-		} catch(e) {
-			dispatch({
-				type: OPER_POST_ERR,
-				payload: e.response.data
-			})
-		}
-	}
-}
+export const deletePost = id => {
+  return async dispatch => {
+    try {
+      const token = Cookies.get("admin_token");
+      const response = await axios.delete(`/api/posts/${id}`, {
+        headers: { "x-auth": `Bearer ${token}` }
+      });
+      dispatch({
+        type: DELETE_POST,
+        payload: response.data
+      });
+    } catch (e) {
+      dispatch({
+        type: OPER_POST_ERR,
+        payload: e.response.data
+      });
+    }
+  };
+};
 
 /** Read all posts */
 export const readPosts = () => {
-	return async(dispatch) => {
-		try {
-			const response = await axios.get('/api/posts');
-			dispatch({
-				type: READ_POSTS,
-				payload: response.data
-			})
-		} catch(e) {
-			dispatch({
-				type: OPER_POST_ERR,
-				payload: e.response.data
-			})
-		}
-	}
-}
+  return async dispatch => {
+    try {
+      const response = await axios.get("/api/posts?all=all");
+      dispatch({
+        type: READ_POSTS,
+        payload: response.data
+      });
+    } catch (e) {
+      dispatch({
+        type: OPER_POST_ERR,
+        payload: e.response.data
+      });
+    }
+  };
+};
 
 /** Read a post by id */
-export const readPost = (id) => {
-	return async(dispatch) => {
-		try {
-			const response = await axios.get(`/api/posts/${id}`);
-			dispatch({
-				type: READ_POST,
-				payload: response.data
-			})
-		} catch(e) {
-			dispatch({
-				type: OPER_POST_ERR,
-				payload: e.response.data
-			})
-		}
-	}
-}
+export const readPost = id => {
+  return async dispatch => {
+    try {
+      const response = await axios.get(`/api/posts/${id}`);
+      dispatch({
+        type: READ_POST,
+        payload: response.data
+      });
+    } catch (e) {
+      dispatch({
+        type: OPER_POST_ERR,
+        payload: e.response.data
+      });
+    }
+  };
+};
 
 /** Update a post by id */
 export const updatePost = (id, data) => {
-	return async(dispatch) => {
-		try {
-			const token = Cookies.get('admin_token');
-			const response = await axios.patch(
-				`/api/posts/${id}`,
-				data,
-				{ headers: {'x-auth': token} }
-			);
-			dispatch({
-				type: UPDATE_POST,
-				payload: response.data
-			});
-		} catch(e) {
-			dispatch({
-				type: OPER_POST_ERR,
-				payload: e.response.data
-			})
-		}
-	}
-}
+  return async dispatch => {
+    try {
+      const token = Cookies.get("admin_token");
+      const response = await axios.patch(`/api/posts/${id}`, data, {
+        headers: { "x-auth": token }
+      });
+      dispatch({
+        type: UPDATE_POST,
+        payload: response.data
+      });
+    } catch (e) {
+      dispatch({
+        type: OPER_POST_ERR,
+        payload: e.response.data
+      });
+    }
+  };
+};
 
 /** Clear data in post reducer */
 export const clearPost = () => {
-	return (dispatch) => {
-		dispatch({
-			type: CLEAR_POST
-		})
-	}
+  return dispatch => {
+    dispatch({
+      type: CLEAR_POST
+    });
+  };
 };
