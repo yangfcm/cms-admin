935ed9ff3474b332705f29c957ac9af062193874
Fan Yang <yangfcm16@gmail.com>
2019-10-17 23:04:41 +1030

    Increase timeout limit

--- src/settings/index.js
+++ src/settings/index.js
@@ -1,6 +1,6 @@
-import axios from 'axios';
+import axios from "axios";
 
 export default axios.create({
-	baseURL: process.env.REACT_APP_API_URL,
-	timeout: 5000,
-})
+  baseURL: process.env.REACT_APP_API_URL,
+  timeout: 10000
+});
