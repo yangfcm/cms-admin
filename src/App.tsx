8861d03c01f22a54aa71402c04832290f37753cb
yangfcm <yangfcm@outlook.com>
2023-03-18 22:31:00 +1030

    Set up redux and reset css.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+import { Provider } from "react-redux";
+import store from "./app/store";
+
 function App() {
-  return <div>App is running</div>;
+  return (
+    <Provider store={store}>
+      <div>App is running</div>
+    </Provider>
+  );
 }
 
 export default App;
---
+++ src/app/store.ts
@@ -0,0 +1,17 @@
+import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
+
+export type AppDispatch = typeof store.dispatch;
+export type RootState = ReturnType<typeof store.getState>;
+export type AppThunk<ReturnType = void> = ThunkAction<
+  ReturnType,
+  RootState,
+  unknown,
+  Action<string>
+>;
+
+const store = configureStore({
+  reducer: {
+  },
+});
+
+export default store;
