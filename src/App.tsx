fb0e746d92239f9f9f4ffdffdd2821efdbf02407
yangfcm <yangfcm@outlook.com>
2023-03-18 22:25:42 +1030

    Re-init project.

---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+function App() {
+  return <div>App is running</div>;
+}
+
+export default App;
--- src/actions/error.js
+++
@@ -1,9 +0,0 @@
-import { CLEAR_ERR} from './types';
-
-export const clearError = () => {
-	return (dispatch) => {
-		dispatch({
-			type: CLEAR_ERR
-		})
-	}
-};
