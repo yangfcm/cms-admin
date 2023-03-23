21f06aba4f4ce0b04368f7e40af630826db5eb96
yangfcm <yangfcm@outlook.com>
2023-03-23 22:49:37 +1030

    Add a button to do sign out.

--- src/pages/Root.tsx
+++ src/pages/Root.tsx
@@ -1,9 +1,12 @@
 import { Outlet } from "react-router-dom";
+import useAuth from "../features/user/useAuth";
 
 function Root() {
+  const { signout } = useAuth();
   return (
     <div>
       <h1>Rootpage</h1>
+      <button onClick={signout}>Sign out</button>
       <Outlet />
     </div>
   );
