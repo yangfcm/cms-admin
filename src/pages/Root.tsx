0f30b4e50f9796c736e6bcaa0e77928203b97226
yangfcm <yangfcm@outlook.com>
2023-04-25 00:34:01 +0930

    Working to show success feedback on blog created.

--- src/pages/Root.tsx
+++ src/pages/Root.tsx
@@ -4,11 +4,18 @@ import Toolbar from "@mui/material/Toolbar";
 import Box from "@mui/material/Box";
 import Sidebar from "../components/Sidebar";
 import Header from "../components/Header";
+import SuccessMessage from "../components/SuccessMessage";
 import useUserBlog from "../features/blog/useUserBlog";
+import { useCreateBlogMutation } from "../features/blog/services";
+import { BLOG_CREATED } from "../settings/constants";
 
 function BlogProvider({ children }: { children: JSX.Element }) {
   const { address } = useParams();
+  // const [createBlog, { isSuccess: blogIsCreated }] = useCreateBlogMutation({
+  //   fixedCacheKey: "createBlogMutationKey",
+  // });
   const { setActiveBlog } = useUserBlog();
+  // console.log(blogIsCreated);
 
   useEffect(() => {
     if (address) {
@@ -16,7 +23,12 @@ function BlogProvider({ children }: { children: JSX.Element }) {
     }
   }, [address]);
 
-  return children;
+  return (
+    <>
+      {/* <SuccessMessage message={BLOG_CREATED} open={blogIsCreated} /> */}
+      {children}
+    </>
+  );
 }
 
 function Root() {
