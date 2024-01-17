7cfca60094a388063880389b3c2091e98723048d
Fan Yang <yangfcm16@gmail.com>
2024-01-17 16:34:07 +1030

    Fix missing types to edit and delete function.

--- src/pages/MuiCategories.tsx
+++ src/pages/MuiCategories.tsx
@@ -19,6 +19,7 @@ import {
   CATEGORY_UPDATED,
   CATEGORY_DELETED,
 } from "../settings/constants";
+import { Category, PostCategory } from "../features/category/types";
 
 function MuiCategories() {
   const { activeBlog } = useUserBlog();
@@ -139,7 +140,7 @@ function MuiCategories() {
           add: {
             labelText: "Add Category",
           },
-          onRowAdd: (newData) => {
+          onRowAdd: (newData: PostCategory) => {
             return new Promise(async (resolve, reject) => {
               const response = await createCategory({
                 blogAddress: address,
@@ -152,7 +153,7 @@ function MuiCategories() {
               }
             });
           },
-          onRowEdit: (editData) => {
+          onRowEdit: (editData: Pick<Category, 'id'> & Partial<PostCategory>) => {
             return new Promise(async (resolve, reject) => {
               const response = await updateCategory({
                 blogAddress: address,
@@ -165,7 +166,7 @@ function MuiCategories() {
               }
             });
           },
-          onRowDelete: (deleteData) => {
+          onRowDelete: (deleteData: Pick<Category, 'id'>) => {
             return new Promise(async (resolve, reject) => {
               const response = await deleteCategory({
                 blogAddress: address,
