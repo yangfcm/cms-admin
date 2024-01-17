4f16f5fb5f9898cea04ee3d59f5eb580505d359b
Fan Yang <yangfcm16@gmail.com>
2024-01-17 16:05:34 +1030

    Add font file and start implementing categories table.

--- src/components/CategoriesTable.tsx
+++ src/components/CategoriesTable.tsx
@@ -1,4 +1,11 @@
-import { Paper } from "@mui/material";
+import Paper from '@mui/material/Paper';
+import Table from '@mui/material/Table';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import TableCell from '@mui/material/TableCell';
+import TableBody from '@mui/material/TableBody';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
 import { Category } from "../features/category/types";
 
 type CategoriesTableProps = {
@@ -6,9 +13,35 @@ type CategoriesTableProps = {
 }
 
 function CategoriesTable(props: CategoriesTableProps) {
+  const { categories } = props;
+
   return (
     <Paper>
-      Cateogires
+      <Toolbar>
+        <Typography variant="h6" sx={{flex: "1 1 100%"}}>
+          Categories List
+        </Typography>
+      </Toolbar>
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell>Name</TableCell>
+            <TableCell>Description</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {
+            categories.map(category => {
+              return (
+                <TableRow key={category.id}>
+                  <TableCell>{category.name}</TableCell>
+                  <TableCell>{category.description}</TableCell>
+                </TableRow>
+              )
+            })
+          }
+        </TableBody>
+      </Table>
     </Paper>
   )
 }
