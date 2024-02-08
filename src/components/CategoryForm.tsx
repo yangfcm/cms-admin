42dde313de7dc06921b08a9708f320136d0b3db7
Fan Yang <yangfcm16@gmail.com>
2024-02-08 23:46:42 +1030

    Add category form.

---
+++ src/components/CategoryForm.tsx
@@ -0,0 +1,65 @@
+import { useForm, FormProvider } from 'react-hook-form';
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Stack from "@mui/material/Stack";
+import { LoadingButton } from '@mui/lab';
+import TextInput from './TextInput';
+import { Category } from '../features/category/types';
+import { CATEGORY_NAME_REQUIRED } from '../settings/constants';
+
+type CategoryFormProps = {
+  category?: Category;
+  onCancel?: () => void;
+};
+function CategoryForm(props: CategoryFormProps) {
+  const { category, onCancel } = props;
+
+  const methods = useForm({
+    mode: 'onSubmit',
+    defaultValues: {
+      name: category? category.name : '',
+      description: category ? category.description : '',
+    }
+  });
+
+  const onSubmit = (data: unknown) => {
+    console.log(data);
+  }
+
+  return (
+    <FormProvider {...methods}>
+      <Box component="form" onSubmit={methods.handleSubmit(onSubmit)}>
+        <TextInput
+          name="name"
+          id="category-name-input"
+          label="Name"
+          placeholder="Name"
+          rules={{
+            required: CATEGORY_NAME_REQUIRED
+          }}
+        />
+        <TextInput
+          name="description"
+          id="category-description-input"
+          label="Description"
+          placeholder="Description"
+        />
+        <Stack
+          direction="row"
+          justifyContent="flex-end"
+          alignItems="center"
+          spacing={1}
+        >
+          <Button type="button" onClick={() => {onCancel && onCancel()}}>Cancel</Button>
+          <LoadingButton
+            type="submit"
+          >
+            Submit
+          </LoadingButton>
+        </Stack>
+      </Box>
+    </FormProvider>
+  )
+}
+
+export default CategoryForm;
