d43e03604178db15eff65a3b01ee41833e99dbd9
Fan Yang <yangfcm16@gmail.com>
2024-02-13 09:20:20 +1030

    Add success feedback and complete Tags page.

--- src/pages/Tags.tsx
+++ src/pages/Tags.tsx
@@ -11,11 +11,16 @@ import {
 import useUserBlog from "../features/blog/useUserBlog";
 import TagsTable from "../components/TagsTable";
 import {
+  TAG_CREATED,
   TAG_CREATE_FIXED_CACHE_KEY,
+  TAG_DELETED,
   TAG_DELETE_FIXED_CACHE_KEY,
+  TAG_UPDATED,
   TAG_UPDATE_FIXED_CACHE_KEY,
 } from "../settings/constants";
 import ErrorMessage from "../components/ErrorMessage";
+import SuccessMessage from "../components/SuccessMessage";
+import Loader from "../components/Loader";
 
 function Tags() {
   const { activeBlogAddress } = useUserBlog();
@@ -67,11 +72,28 @@ function Tags() {
   return (
     <Container>
       <ErrorMessage open={hasError} messages={errorMessages} onClose={reset} />
+      <SuccessMessage
+        open={createTagState.isSuccess}
+        message={TAG_CREATED}
+        onClose={createTagState.reset}
+      />
+      <SuccessMessage
+        open={updateTagState.isSuccess}
+        message={TAG_UPDATED}
+        onClose={updateTagState.reset}
+      />
+      <SuccessMessage
+        open={deleteTagState.isSuccess}
+        message={TAG_DELETED}
+        onClose={deleteTagState.reset}
+      />
       <Typography variant="h5" sx={{ marginBottom: 1 }}>
         Tags Admin
       </Typography>
       <Divider />
-      <TagsTable tags={data?.tags || []} />
+      <br />
+      {isReadingTags && <Loader />}
+      {!isReadingTags && <TagsTable tags={data?.tags || []} />}
     </Container>
   );
 }
