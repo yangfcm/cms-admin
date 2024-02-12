1aa6b6b67b7799d0f3ebf5c1ebb5916a8953a69d
Fan Yang <yangfcm16@gmail.com>
2024-02-13 10:06:02 +1030

    Add article type.

---
+++ src/features/article/types.ts
@@ -0,0 +1,31 @@
+import { Blog } from "../blog/types";
+import { Category } from "../category/types";
+import { Tag } from "../tag/types";
+import { User } from "../user/types";
+
+export enum ArticleStatus {
+  DRAFT = "draft",
+  PUBLISHED = "published",
+  TRASH = "trash",
+}
+
+export type Article = {
+  id: string;
+  title: string;
+  content: string;
+  featuredImage: string;
+  status: ArticleStatus;
+  isTop: boolean;
+  blog: Pick<Blog, "id" | "title" | "address">;
+  user: Pick<User, "id" | "username" | "nickname" | "biography" | "avatar">;
+  category: Pick<Category, "id" | "name" | "description">;
+  tags: Pick<Tag, "id" | "name">[];
+};
+
+export type ArticleResponse = {
+  article: Article;
+};
+
+export type ArticlesResponse = {
+  articles: Article[];
+};
