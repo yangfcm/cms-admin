1f67c5643a77b12bc548ad01c9717cc175f612e1
Fan Yang <yangfcm16@gmail.com>
2024-03-05 10:29:52 +1030

    Add more styles to multi-select input.

--- src/components/inputs/MultiSelectInput.tsx
+++ src/components/inputs/MultiSelectInput.tsx
@@ -3,6 +3,9 @@ import {
   UseControllerProps,
   Controller,
 } from "react-hook-form";
+import { Theme, useTheme } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import Chip from "@mui/material/Chip";
 import FormControl from "@mui/material/FormControl";
 import InputLabel from "@mui/material/InputLabel";
 import Select from "@mui/material/Select";
@@ -14,10 +17,24 @@ type MultiSelectInputProps = UseControllerProps & {
   options: { value: string | number; label: string }[];
 };
 
+function getStyles(option: string, selected: string[], theme: Theme) {
+  return {
+    fontWeight:
+      selected.indexOf(option) === -1
+        ? theme.typography.fontWeightRegular
+        : theme.typography.fontWeightMedium,
+  };
+}
+
 function MultiSelectInput(props: MultiSelectInputProps) {
+  const theme = useTheme();
   const { id, label = "Multi-select", name, options } = props;
   const formContext = useFormContext();
   const { control } = formContext;
+  const optionsObj = options.reduce((result, item) => {
+    result[item.value as string] = item.label;
+    return result;
+  }, {} as Record<string, string>);
 
   return (
     <FormControl fullWidth>
@@ -27,9 +44,29 @@ function MultiSelectInput(props: MultiSelectInputProps) {
         control={control}
         defaultValue={[]} /** @TODO Fix defaultValue. */
         render={({ field }) => (
-          <Select labelId={id} label={label} multiple {...field}>
+          <Select
+            labelId={id}
+            label={label}
+            multiple
+            renderValue={(selected: string[]) => (
+              <Box sx={{ display: "flex", flexWrap: "wrap", gap: 0.5 }}>
+                {selected.map((value) => (
+                  <Chip
+                    key={value}
+                    label={optionsObj[value]}
+                    size="small"
+                  ></Chip>
+                ))}
+              </Box>
+            )}
+            {...field}
+          >
             {options.map((option) => (
-              <MenuItem key={option.value} value={option.value}>
+              <MenuItem
+                key={option.value}
+                value={option.value}
+                style={getStyles(option.value as string, field.value, theme)}
+              >
                 {option.label}
               </MenuItem>
             ))}
