77277ba39ee9e6c2433760ebecbf1df5e91e8035
Fan Yang <yangfcm16@gmail.com>
2024-03-09 00:05:22 +1030

    Populate article form when editing articles.

--- src/pages/EditArticle.tsx
+++ src/pages/EditArticle.tsx
@@ -1,12 +1,23 @@
+import { useParams } from "react-router-dom";
 import Container from "@mui/material/Container";
 import PageTitle from "../components/PageTitle";
 import ArticleForm from "../components/forms/ArticleForm";
+import { useReadArticleQuery } from "../features/article/services";
+import useUserBlog from "../features/blog/useUserBlog";
 
 function EditArticle() {
+  const { id = "" } = useParams();
+  const { activeBlogAddress: blogAddress } = useUserBlog();
+  const { data: { article } = {} } = useReadArticleQuery({ blogAddress, id });
+
+  if (!article) {
+    return <Container>Loading...</Container>;
+  }
+
   return (
     <Container>
       <PageTitle title="Edit Article" />
-      <ArticleForm />
+      <ArticleForm article={article} />
     </Container>
   );
 }
