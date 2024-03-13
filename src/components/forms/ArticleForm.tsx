e02b32a15433a1900413eb83f33e613d7ee0b5ec
Fan Yang <yangfcm16@gmail.com>
2024-03-13 21:36:28 +1030

    Update article form to emit create/update article success or error event.

--- src/components/forms/ArticleForm.tsx
+++ src/components/forms/ArticleForm.tsx
@@ -1,4 +1,4 @@
-import { useCallback } from "react";
+import { useCallback, useEffect } from "react";
 import { useForm, FormProvider } from "react-hook-form";
 import Box from "@mui/material/Box";
 import { LoadingButton } from "@mui/lab";
@@ -11,13 +11,21 @@ import EditorInput from "../inputs/EditorInput";
 import SwitchInput from "../inputs/SwitchInput";
 import SelectInput from "../inputs/SelectInput";
 import MultiSelectInput from "../inputs/MultiSelectInput";
-import { useCreateArticleMutation } from "../../features/article/services";
+import {
+  useCreateArticleMutation,
+  useUpdateArticleMutation,
+} from "../../features/article/services";
 import { useReadCategoriesQuery } from "../../features/category/services";
 import { useReadTagsQuery } from "../../features/tag/services";
 import { ARTICLE_TITLE_REQUIRED } from "../../settings/constants";
+import parseError from "../../utils/parseError";
 
 type ArticleFormProps = {
   article?: Article;
+  onCreateArticleSuccess?: () => void;
+  onUpdateArticleSuccess?: () => void;
+  onCreateArticleError?: (error: string | string[]) => void;
+  onUpdateArticleError?: (error: string | string[]) => void;
 };
 
 type ArticleFormData = Pick<
@@ -26,14 +34,38 @@ type ArticleFormData = Pick<
 > & { categoryId: string; tagIds: string[]; isDraft: boolean };
 
 function ArticleForm(props: ArticleFormProps) {
-  const { article } = props;
+  const {
+    article,
+    onCreateArticleSuccess,
+    onUpdateArticleSuccess,
+    onCreateArticleError,
+    onUpdateArticleError,
+  } = props;
 
   const { activeBlogAddress } = useUserBlog();
   const { data: { categories = [] } = {} } =
     useReadCategoriesQuery(activeBlogAddress);
   const { data: { tags = [] } = {} } = useReadTagsQuery(activeBlogAddress);
 
-  const [createArticle, { isLoading, isSuccess }] = useCreateArticleMutation();
+  const [
+    createArticle,
+    {
+      isLoading: isCreatingArticle,
+      isSuccess: isCreateArticleSuccess,
+      isError: isCreateArticleError,
+      error: createArticleError,
+    },
+  ] = useCreateArticleMutation();
+
+  const [
+    updateArticle,
+    {
+      isLoading: isUpdatingArticle,
+      isSuccess: isUpdateArticleSuccess,
+      isError: isUpdateArticleError,
+      error: updateArticleError,
+    },
+  ] = useUpdateArticleMutation();
 
   const methods = useForm({
     mode: "onSubmit",
@@ -49,15 +81,52 @@ function ArticleForm(props: ArticleFormProps) {
     },
   });
 
-  const onSubmit = useCallback((data: ArticleFormData) => {
-    createArticle({
-      blogAddress: activeBlogAddress,
-      article: {
+  const onSubmit = useCallback(
+    (data: ArticleFormData) => {
+      const newArticleData = {
         ...data,
         status: data.isDraft ? ArticleStatus.DRAFT : ArticleStatus.PUBLISHED,
-      },
-    });
-  }, []);
+      };
+      if (!!article) {
+        updateArticle({
+          blogAddress: activeBlogAddress,
+          article: {
+            ...newArticleData,
+            id: article.id,
+          },
+        });
+      } else {
+        createArticle({
+          blogAddress: activeBlogAddress,
+          article: newArticleData,
+        });
+      }
+    },
+    [activeBlogAddress]
+  );
+
+  useEffect(() => {
+    if (isCreateArticleSuccess && onCreateArticleSuccess) {
+      onCreateArticleSuccess();
+    }
+    if (isUpdateArticleSuccess && onUpdateArticleSuccess) {
+      onUpdateArticleSuccess();
+    }
+  }, [isCreateArticleSuccess, isUpdateArticleSuccess]);
+
+  useEffect(() => {
+    if (isCreateArticleError && onCreateArticleError) {
+      onCreateArticleError(parseError(createArticleError));
+    }
+    if (isUpdateArticleError && onUpdateArticleError) {
+      onUpdateArticleError(parseError(updateArticleError));
+    }
+  }, [
+    isCreateArticleError,
+    createArticleError,
+    isUpdateArticleError,
+    updateArticleError,
+  ]);
 
   return (
     <FormProvider {...methods}>
@@ -129,7 +198,12 @@ function ArticleForm(props: ArticleFormProps) {
           alignItems="center"
           spacing={1}
         >
-          <LoadingButton type="submit" variant="contained" size="large">
+          <LoadingButton
+            loading={isCreatingArticle || isUpdatingArticle}
+            type="submit"
+            variant="contained"
+            size="large"
+          >
             Publish
           </LoadingButton>
         </Stack>
