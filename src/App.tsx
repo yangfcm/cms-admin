575ebcf201ad0f549d050d4717faafa2ec72cc58
Fan Yang <yangfcm16@gmail.com>
2024-03-15 22:55:43 +1030

    Delete unwanted code.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
 import { Provider } from "react-redux";
 import store from "./app/store";
-import Box from "@mui/material/Box";
 import AppThemeProvider from "./components/AppThemeProvider";
 import AppRoutes from "./routes/AppRoutes";
-import SuccessMessage from "./components/SuccessMessage";
 import SnackbarProvider from "./components/SnackbarProvider";
 
 function App() {
@@ -13,30 +11,6 @@ function App() {
         <SnackbarProvider>
           <AppRoutes />
         </SnackbarProvider>
-        {/* <Box
-          sx={{
-            zIndex: 1400,
-            position: "fixed",
-            left: "50%",
-            bottom: 0,
-          }}
-        >
-          <SuccessMessage
-            open={true}
-            message="success1"
-            sx={{ position: "static" }}
-          />
-          <SuccessMessage
-            open={true}
-            message="success2"
-            sx={{ position: "static" }}
-          />
-          <SuccessMessage
-            open={true}
-            message="success3"
-            sx={{ position: "static" }}
-          />
-        </Box> */}
       </AppThemeProvider>
     </Provider>
   );
